a3d8da045ef66e9cf9bac76b8dbea8672a14f2eb
Chamika Dilshan <chamidilshan55@gmail.com>
2024-02-18 21:09:49 +0530

    added hf model

---
+++ pages/api/huggingface/index.js
@@ -0,0 +1,26 @@
+import { HfInference } from "@huggingface/inference";
+
+const HF_ACCESS_TOKEN = process.env.HF_ACCESS_TOKEN;
+
+const inference = new HfInference(HF_ACCESS_TOKEN);
+
+export default async function handler(req, res) {
+  const { text, lang } = req.body;
+
+  const languageModels = {
+    "en-es": "Helsinki-NLP/opus-mt-en-es",
+    "ec-de": "Helsinki-NLP/opus-mt-en-de",
+    "en-fr": "Helsinki-NLP/opus-mt-en-fr",
+  }
+
+  const response = await inference.translation({
+    model: languageModels[lang],
+    inputs: text,
+  });
+
+  res.status(200).json({
+    translated_text: response.translation_text,
+  }); 
+  
+}
+
